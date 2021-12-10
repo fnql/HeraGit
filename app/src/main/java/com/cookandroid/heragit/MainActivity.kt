7f0ef21223011e00e4240d16a7e194e847ff77a5
woogi <dbwhddnr00@naver.com>
2021-12-10 17:41:26 +0900

    logo 완성

--- app/src/main/java/com/cookandroid/heragit/MainActivity.kt
+++ app/src/main/java/com/cookandroid/heragit/MainActivity.kt
@@ -2,10 +2,17 @@ package com.cookandroid.heragit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.logo)
+            .setContentTitle(textTitle)
+            .setContentText(textContent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
     }
 }
