6ecd343f231342c6dff2fba4fa7b6a6bfb363de0
woogi <dbwhddnr00@naver.com>
2021-12-14 14:33:31 +0900

    기본알림 완성

--- app/src/main/java/com/cookandroid/heragit/MainActivity.kt
+++ app/src/main/java/com/cookandroid/heragit/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,59 +13,48 @@ import androidx.core.app.NotificationManagerCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var textTitle = "Do github"
+    var textContent = "오늘 커밋이 없어요"
+    var CHANNEL_ID = "MYch"
+    var CHANNEL_NAME = "ch1"
+    var notificationId:Int = 1002
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
         button.setOnClickListener {
-            var textTitle = "Do github"
-            var textContent = "오늘 커밋이 없어요"
-            var CHANNEL_ID = "0"
             var builder = NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.logo)
                 .setContentTitle(textTitle)
                 .setContentText(textContent)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            with(NotificationManagerCompat.from(this)) {
-                // notificationId is a unique int for each notification that you must define
-                notify(0, builder.build())
-            }
+
+            createNotificationChannel(builder,notificationId)
         }
 
-
-//        // Create an explicit intent for an Activity in your app
-//        val intent = Intent(this, AlertDetails::class.java).apply {
-//            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-//        }
-//        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
-//
-//        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-//            .setSmallIcon(R.drawable.notification_icon)
-//            .setContentTitle("My notification")
-//            .setContentText("Hello World!")
-//            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-//            // Set the intent that will fire when the user taps the notification
-//            .setContentIntent(pendingIntent)
-//            .setAutoCancel(true)
-//    //Todo: 채널 이름,아이디 만드는법
-
     }
-    private fun createNotificationChannel() {
+    private fun createNotificationChannel(builder:NotificationCompat.Builder,notificationId:Int) {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = getString(R.string.channel_name)
-            val descriptionText = getString(R.string.channel_description)
+            val descriptionText = "1번 채널"
             val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance).apply {
                 description = descriptionText
             }
-            // Register the channel with the system
+            channel.lightColor = Color.BLUE
+            channel.enableVibration(true)
             val notificationManager: NotificationManager =
                 getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
+
+            notificationManager.notify(notificationId,builder.build())
+        }else{
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(notificationId,builder.build())
         }
     }
-    https://ddolcat.tistory.com/580
 }
