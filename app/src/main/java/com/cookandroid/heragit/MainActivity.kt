15ba50ad7db3866d7eb681850c983dcfa18a44a0
woogi <dbwhddnr00@naver.com>
2021-12-17 12:25:50 +0900

    asyncTask error

--- app/src/main/java/com/cookandroid/heragit/MainActivity.kt
+++ app/src/main/java/com/cookandroid/heragit/MainActivity.kt
@@ -5,21 +5,17 @@ import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
-import android.os.Build
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import kotlinx.android.synthetic.main.activity_main.*
-import android.os.AsyncTask
 import java.io.InputStream
 import java.io.InputStreamReader
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
 import android.util.JsonReader
-
-
-
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,9 +24,8 @@ class MainActivity : AppCompatActivity() {
     var CHANNEL_ID = "MYch"
     var CHANNEL_NAME = "ch1"
     var notificationId:Int = 1002
-    var githubEndpoint: URL = URL("https://api.github.com/")
-    var myConnection: HttpsURLConnection = githubEndpoint.openConnection() as HttpsURLConnection
 
+//    https://code.tutsplus.com/ko/tutorials/android-from-scratch-using-rest-apis--cms-27117
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,14 +42,30 @@ class MainActivity : AppCompatActivity() {
             createNotificationChannel(builder,notificationId)
         }
 
-        AsyncTask.execute {
+        api_btn.setOnClickListener {
+            var data = getApi()
+        }
+
+
+
+    }
+
+    private fun getApi(): Any {
+        var githubEndpoint: URL = URL("https://api.github.com/")
+        var myConnection: HttpsURLConnection = githubEndpoint.openConnection() as HttpsURLConnection
+
+        return AsyncTask.execute {
             // All your networking logic
             // should be here
             myConnection.setRequestProperty("User-Agent", "my-rest-app-v0.1");
-            myConnection.setRequestProperty("Accept",
-                "application/vnd.github.v3+json");
-            myConnection.setRequestProperty("Contact-Me",
-                "dbwhddnr00@naver.com");
+            myConnection.setRequestProperty(
+                "Accept",
+                "application/vnd.github.v3+json"
+            );
+            myConnection.setRequestProperty(
+                "Contact-Me",
+                "dbwhddnr00@naver.com"
+            );
             if (myConnection.getResponseCode() == 200) {
                 val responseBody: InputStream = myConnection.inputStream
                 var responseBodyReader: InputStreamReader = InputStreamReader(responseBody, "UTF-8")
@@ -66,7 +77,11 @@ class MainActivity : AppCompatActivity() {
                     if (key == "organization_url") { // Check if desired key
                         // Fetch the value as a String
                         val value = jsonReader.nextString()
-
+                        val handler = Handler(Looper.getMainLooper())
+                        handler.postDelayed(Runnable {
+                            value
+                        }, 0)
+                        Toast.makeText(this,value,Toast.LENGTH_LONG).show()
                         // Do something with the value
                         // ...
                         break // Break out of the loop
@@ -80,8 +95,8 @@ class MainActivity : AppCompatActivity() {
                 // Error handling code goes here
             }
         }
-
     }
+
     private fun createNotificationChannel(builder:NotificationCompat.Builder,notificationId:Int) {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
