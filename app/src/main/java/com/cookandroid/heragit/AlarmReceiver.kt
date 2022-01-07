4ebf07fd0193a1914d0a8a7502bcd89ac18ad96a
fnql <dbwhddnr00@naver.com>
2022-01-07 14:52:09 +0900

    알림로직+ alarm

--- app/src/main/java/com/cookandroid/heragit/AlarmReceiver.kt
+++ app/src/main/java/com/cookandroid/heragit/AlarmReceiver.kt
@@ -1,15 +1,66 @@
 package com.cookandroid.heragit
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat.getSystemService
 
 class AlarmReceiver : BroadcastReceiver() {
+    var textTitle = "Do github"
+    var textContent = "오늘 커밋이 없어요"
+    var CHANNEL_ID = "MYch"
+    var CHANNEL_NAME = "ch1"
+    var notificationId:Int = 1002
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if(intent != null){
+            createNoGit()
+            Log.e("알람", System.currentTimeMillis().toString())
+            Toast.makeText(context,"alram~~",Toast.LENGTH_SHORT).show()
+        }
+        val notificationManager =
+            context!!.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationIntent = Intent(context, MainActivity::class.java)
 
-    override fun onReceive(context: Context, intent: Intent) {
-        Toast.makeText(context,"alram~~",Toast.LENGTH_SHORT).show()
-        Log.e("Alarm","알람입니다.")
+    }
+
+    private fun createNotificationChannel(builder: NotificationCompat.Builder, notificationId:Int) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val descriptionText = "1번 채널"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance).apply {
+                description = descriptionText
+            }
+            channel.lightColor = Color.BLUE
+            channel.enableVibration(true)
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+
+            notificationManager.notify(notificationId,builder.build())
+        }else{
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(notificationId,builder.build())
+        }
+    }
+    private fun createNoGit() {
+        var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.logo)
+            .setContentTitle(textTitle)
+            .setContentText(textContent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        createNotificationChannel(builder,notificationId)
     }
 }
--- app/src/main/java/com/cookandroid/heragit/MainActivity.kt
+++ app/src/main/java/com/cookandroid/heragit/MainActivity.kt
@@ -29,14 +29,9 @@ import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-    var textTitle = "Do github"
-    var textContent = "오늘 커밋이 없어요"
-    var CHANNEL_ID = "MYch"
-    var CHANNEL_NAME = "ch1"
-    var notificationId:Int = 1002
+
     val url = URL("https://api.github.com/users/fnql/events?per_page=1")
-    private var alarmMgr: AlarmManager? = null
-    private lateinit var alarmIntent: PendingIntent
+
 
 /*  BuildConfig.GITHUB_API_KEY
     https://code.tutsplus.com/ko/tutorials/android-from-scratch-using-rest-apis--cms-27117
@@ -50,13 +45,7 @@ class MainActivity : AppCompatActivity() {
         val TAG:String = "MainActivity : "
 
         button.setOnClickListener {
-            var builder = NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.logo)
-                .setContentTitle(textTitle)
-                .setContentText(textContent)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
-            createNotificationChannel(builder,notificationId)
         }
 
         api_btn.setOnClickListener {
@@ -99,9 +88,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         alarm_start.setOnClickListener {
-
-            alarmMgr = this.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-            alarmIntent = Intent(this, AlarmReceiver::class.java).let { intent ->
+            var alarmMgr = this.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            var alarmIntent = Intent(this, AlarmReceiver::class.java).let { intent ->
                 PendingIntent.getBroadcast(this, 0, intent, 0)
             }
             val hour: Int = timer.getHour()
@@ -122,14 +110,10 @@ class MainActivity : AppCompatActivity() {
 
         }
         alarm_cancel.setOnClickListener {
-            val alarmManager =
-                this.getSystemService(Context.ALARM_SERVICE) as? AlarmManager
-            val pendingIntent =
-                PendingIntent.getService(this, requestId, intent,
-                    PendingIntent.FLAG_NO_CREATE)
-            if (pendingIntent != null && alarmManager != null) {
-                alarmManager.cancel(pendingIntent)
-            }
+            var alarmMgr = this.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            var intent = Intent(this, AlarmReceiver::class.java)
+            var pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0)
+            alarmMgr.cancel(pendingIntent)
             Toast.makeText(
                 this,
                 "Alarm 취소",
@@ -138,33 +122,11 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+
 //https://calvinjmkim.tistory.com/16
-    //콜 함수 데이터 메인에서 쓰는법
 
-    private fun createNotificationChannel(builder:NotificationCompat.Builder,notificationId:Int) {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 
-            val descriptionText = "1번 채널"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance).apply {
-                description = descriptionText
-            }
-            channel.lightColor = Color.BLUE
-            channel.enableVibration(true)
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-
-            notificationManager.notify(notificationId,builder.build())
-        }else{
-            // Register the channel with the system
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.notify(notificationId,builder.build())
-        }
-    }
 
     private fun onNetworkFinished(result: String) {
         var gson = Gson()
