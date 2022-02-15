f2cb6f31c0eb70df0c97298cace302b6c0f1de87
woogi <dbwhddnr00@naver.com>
2022-02-15 22:04:56 +0900

    import R 에러 수정

--- app/src/main/java/com/cookandroid/heragit/Gitcallback.kt
+++ app/src/main/java/com/cookandroid/heragit/Gitcallback.kt
@@ -2,16 +2,22 @@ package com.cookandroid.heragit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.net.Uri
+
 
 class Gitcallback : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gitcallback)
     }
-//콜백 함수 받아서 code값만 사용하기
+
     override fun onResume() {
         super.onResume()
-
+        val CALLBACK_URL = getString(R.string.github_callback)
+        val uri: Uri? = intent.data
+        if (uri != null && uri.toString().startsWith(CALLBACK_URL)) {
+            val access_token: String? = uri.getQueryParameter("access_token")
+        }
     }
 
 }
