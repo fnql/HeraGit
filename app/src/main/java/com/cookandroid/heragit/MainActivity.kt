1b744e68565f580cbcd17389bfc224ff52cf11b3
woogi <dbwhddnr00@naver.com>
2022-02-20 23:20:25 +0900

    uri 받아오는중

--- app/src/main/java/com/cookandroid/heragit/MainActivity.kt
+++ app/src/main/java/com/cookandroid/heragit/MainActivity.kt
@@ -13,16 +13,21 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.browser.customtabs.CustomTabsIntent
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.GlobalScope.coroutineContext
 import java.net.URL
 import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.util.*
-
+import kotlinx.coroutines.launch
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
 /*  BuildConfig.GITHUB_API_KEY*/
 //TODO: git while(당일) 당일 커밋여부 확인 - 다른 이름으로 커밋할 수 있
+    //안드로이도 HeraGit://github-auth
 
 //http://localhost:8080/auth/Heragit//ok 사이트 사용 가능한걸로 해야하나?
     @RequiresApi(Build.VERSION_CODES.M)
@@ -70,68 +75,77 @@ class MainActivity : AppCompatActivity() {
         }
         github_apps.setOnClickListener {
             //링크타고 콜랙url에서 뒤code값 get요청으로 받으면 될듯
-            val intent=Intent(Intent.ACTION_VIEW,Uri.parse("https://github.com/login/oauth/authorize?scope=repo,user&client_id=2f977b3d7307952e0d33"))
-            startActivity(intent)
+            /*val intent=Intent(Intent.ACTION_VIEW,Uri.parse("https://github.com/login/oauth/authorize?scope=repo,user&client_id=2f977b3d7307952e0d33"))
+            startActivity(intent)*/
+            login(this)
 
         }
 
     }
-    override fun onResume() {
-        super.onResume()
-        val CALLBACK_URL = getString(R.string.github_callback)
-        //intent.data null 오류
-        val uri: Uri? = intent.data
-        Log.e("onResume",uri.toString())
-        if (uri != null && uri.toString().startsWith(CALLBACK_URL)) {
-            val access_token: String? = uri.getQueryParameter("access_token")
 
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun alarmSetting(){
+        val hour: Int = timer.getHour()
+        val minute: Int = timer.getMinute()
+        val calendar: Calendar = Calendar.getInstance().apply {
+            timeInMillis = System.currentTimeMillis()
+            set(Calendar.HOUR_OF_DAY, hour)
+            set(Calendar.MINUTE, minute)
+        }
+        if (calendar.before(Calendar.getInstance())){
+            calendar.add(Calendar.DATE,1)
+        }
+        val currentDateTime=calendar.getTime()
+        val date_text = SimpleDateFormat("yyyy년 MM월 dd일 EE요일 a hh시 mm분",Locale.getDefault()).format(currentDateTime)
+        Toast.makeText(this,"***다음 알람은 " + date_text+"입니다.",Toast.LENGTH_SHORT).show()
+        val editor = getSharedPreferences("daily", MODE_PRIVATE).edit()
+        editor.putLong("nextDate",calendar.timeInMillis)
+
+        diaryAlarm(calendar)
+    }
+
+    private fun diaryAlarm(calendar: Calendar) {
+        val diaryAl:Boolean = true
+        val pm = this.packageManager
+        val receiver = ComponentName(this,DeviceBootReceiver::class.java)
+        var alarmMgr = this.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        var alarmIntent = Intent(this, AlarmReceiver::class.java).let { intent ->
+            PendingIntent.getBroadcast(this, 0, intent,  0)
+        }
+
+        if (diaryAl){
+            alarmMgr?.setInexactRepeating(
+                AlarmManager.RTC_WAKEUP,
+                calendar.timeInMillis,
+                AlarmManager.INTERVAL_DAY,
+                alarmIntent
+            )
+
+            pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP)
         }
     }
-            @RequiresApi(Build.VERSION_CODES.M)
-            private fun alarmSetting(){
-                val hour: Int = timer.getHour()
-                val minute: Int = timer.getMinute()
-                val calendar: Calendar = Calendar.getInstance().apply {
-                    timeInMillis = System.currentTimeMillis()
-                    set(Calendar.HOUR_OF_DAY, hour)
-                    set(Calendar.MINUTE, minute)
-                }
-                if (calendar.before(Calendar.getInstance())){
-                    calendar.add(Calendar.DATE,1)
-                }
-                val currentDateTime=calendar.getTime()
-                val date_text = SimpleDateFormat("yyyy년 MM월 dd일 EE요일 a hh시 mm분",Locale.getDefault()).format(currentDateTime)
-                Toast.makeText(this,"***다음 알람은 " + date_text+"입니다.",Toast.LENGTH_SHORT).show()
-                val editor = getSharedPreferences("daily", MODE_PRIVATE).edit()
-                editor.putLong("nextDate",calendar.timeInMillis)
-
-                diaryAlarm(calendar)
-            }
-
-            private fun diaryAlarm(calendar: Calendar) {
-                val diaryAl:Boolean = true
-                val pm = this.packageManager
-                val receiver = ComponentName(this,DeviceBootReceiver::class.java)
-                var alarmMgr = this.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-                var alarmIntent = Intent(this, AlarmReceiver::class.java).let { intent ->
-                    PendingIntent.getBroadcast(this, 0, intent,  0)
-                }
-
-                if (diaryAl){
-                    alarmMgr?.setInexactRepeating(
-                        AlarmManager.RTC_WAKEUP,
-                        calendar.timeInMillis,
-                        AlarmManager.INTERVAL_DAY,
-                        alarmIntent
-                    )
-
-                    pm.setComponentEnabledSetting(receiver,
-                        PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
-                        PackageManager.DONT_KILL_APP)
-                }
-            }
+    fun login(context: Context){
+        val loginUrl=Uri.Builder().scheme("https").authority("github.com")
+            .appendPath("login")
+            .appendPath("oauth")
+            .appendPath("authorize")
+            .appendQueryParameter("client_id",BuildConfig.GITHUB_CLIENT_ID)
+            .build()
+        CustomTabsIntent.Builder().build().also {
+            it.launchUrl(context,loginUrl)
         }
+    }
 
-
-
-
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        intent?.data?.getQueryParameter("code")?.let {
+            Log.e("onResume",it)
+            /*launch(coroutineContext){
+                viewModel.getAccessToken(it)
+                Toast.makeText(this@MainActivity,"로그인 완료",Toast.LENGTH_SHORT).show()
+            }*/
+        }
+    }
+}
