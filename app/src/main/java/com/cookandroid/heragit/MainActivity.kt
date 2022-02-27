c67186838757494b584449fa7e1eaed04817b665
woogi <dbwhddnr00@naver.com>
2022-02-27 15:38:10 +0900

    viewmodel1

--- app/src/main/java/com/cookandroid/heragit/MainActivity.kt
+++ app/src/main/java/com/cookandroid/heragit/MainActivity.kt
@@ -146,8 +146,7 @@ class MainActivity : AppCompatActivity() {
             // 엑세스 토큰 받아와야함 get불가능
             // launch말고 다른거
 /*            launch(coroutineContext) {
-                val gitcallback = ?
-                gitcallback.getAccessToken(it)
+                viewmodel.getAccessToken(it)
                 Toast.makeText(this@MainActivity, "로그인 되었습니다.", Toast.LENGTH_SHORT).show()
             }*/
         }
