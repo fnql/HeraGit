b12247bc0ef07d3558a95cccabf787e32c238701
woogi <dbwhddnr00@naver.com>
2022-03-04 10:30:20 +0900

    onNewIntent 이동

--- app/src/main/java/com/cookandroid/heragit/Gitcallback.kt
+++ app/src/main/java/com/cookandroid/heragit/Gitcallback.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.net.Uri
+import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -11,7 +12,19 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 
-class GitCallBack : ViewModel() {
+class GitCallBack : AppCompatActivity() {
 
-
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        Log.e("onNewIntent",intent?.data.toString())
+        intent?.data?.getQueryParameter("code")?.let {
+            val gitcode= it
+            Log.e("onNewIntent",gitcode)
+            // 엑세스 토큰 받아와야함
+            launch(GlobalScope.coroutineContext) {
+                viewmodel.getAccessToken(it)
+                Toast.makeText(this@MainActivity, "로그인 되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
--- app/src/main/java/com/cookandroid/heragit/MainActivity.kt
+++ app/src/main/java/com/cookandroid/heragit/MainActivity.kt
@@ -137,17 +137,5 @@ class MainActivity : AppCompatActivity() {
 
     //https://example.com/callback
     //onResume?
-    override fun onNewIntent(intent: Intent?) {
-        super.onNewIntent(intent)
-        Log.e("onNewIntent",intent?.data.toString())
-        intent?.data?.getQueryParameter("code")?.let {
-            val gitcode= it
-            Log.e("onNewIntent",gitcode)
-            // 엑세스 토큰 받아와야함
-            launch(coroutineContext) {
-                viewmodel.getAccessToken(it)
-                Toast.makeText(this@MainActivity, "로그인 되었습니다.", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
+
 }
