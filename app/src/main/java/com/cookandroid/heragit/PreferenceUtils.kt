6d08ba3d87d07c70255d8c5edf386ecf1687dd3e
woogi <dbwhddnr00@naver.com>
2022-04-25 11:03:41 +0900

    nextDate 조정중

--- app/src/main/java/com/cookandroid/heragit/PreferenceUtils.kt
+++ app/src/main/java/com/cookandroid/heragit/PreferenceUtils.kt
@@ -6,9 +6,14 @@ import android.content.SharedPreferences
 class PreferenceUtils(context: Context) {
     private val prefsFilename = "prefs"
     private val prefsKeyEdt = "myEditText"
+    private val prefsKeyDaily = "현재시간변수"
     private val prefs: SharedPreferences = context.getSharedPreferences(prefsFilename, 0)
 
     var myEditText: String?
+        get() = prefs.getString(prefsKeyEdt, "")
+        set(value) = prefs.edit().putString(prefsKeyEdt, value).apply()
+
+    var dailyTime: String?
         get() = prefs.getString(prefsKeyEdt, "")
         set(value) = prefs.edit().putString(prefsKeyEdt, value).apply()
 }
