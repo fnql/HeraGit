220c1f8c1dc178c9ecea53fb95034285aa88ce4c
woogi <dbwhddnr00@naver.com>
2022-04-26 22:17:05 +0900

    date 자료형 사용법

--- app/src/main/java/com/cookandroid/heragit/PreferenceUtils.kt
+++ app/src/main/java/com/cookandroid/heragit/PreferenceUtils.kt
@@ -2,18 +2,20 @@ package com.cookandroid.heragit
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.widget.DatePicker
+import java.util.*
 
 class PreferenceUtils(context: Context) {
     private val prefsFilename = "prefs"
     private val prefsKeyEdt = "myEditText"
-    private val prefsKeyDaily = "현재시간변수"
+    private val prefsKeyDaily =
     private val prefs: SharedPreferences = context.getSharedPreferences(prefsFilename, 0)
 
     var myEditText: String?
         get() = prefs.getString(prefsKeyEdt, "")
         set(value) = prefs.edit().putString(prefsKeyEdt, value).apply()
 
-    var dailyTime: String?
-        get() = prefs.getString(prefsKeyEdt, "")
+    var dailyTime: Date?
+        get() = prefs.getDate(prefsKeyDaily, )
         set(value) = prefs.edit().putString(prefsKeyEdt, value).apply()
 }
